ecb4a6d762db9268ceaf6edabac73c6a3b78e181
sguo1995 <sguo1403@gmail.com>
2019-02-22 12:49:00 -0500

    Print Log

--- app/src/main/java/com/example/zhangyujia/asd/StartActivity.java
+++ app/src/main/java/com/example/zhangyujia/asd/StartActivity.java
@@ -3,6 +3,7 @@ package com.example.zhangyujia.asd;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -11,6 +12,7 @@ public class StartActivity extends AppCompatActivity {
 
 
 
+    public static final String TAG="StartActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,5 +40,36 @@ public class StartActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG,"onDestroy");
+    }
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+        Log.d(TAG,"onRestart");
     }
 }
