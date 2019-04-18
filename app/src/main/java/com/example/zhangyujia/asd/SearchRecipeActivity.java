48c8b5581083ca3a5aed0a44b0622fbdbeb909e7
zzpudding <yujiazhang_@outlook.com>
2019-04-18 01:12:47 -0400

    Fix search bugs

--- app/src/main/java/com/example/zhangyujia/asd/SearchRecipeActivity.java
+++ app/src/main/java/com/example/zhangyujia/asd/SearchRecipeActivity.java
@@ -17,87 +17,96 @@ import org.litepal.LitePal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.lang.String;
 
-public class SearchRecipeActivity extends AppCompatActivity implements View.OnClickListener{
+public class SearchRecipeActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private static final String TAG="SearchActivity";
+    private static final String TAG = "SearchActivity";
     private EditText mSearchArea;
     private RecyclerView mSearchResults;
     private ImageButton mSearchBtn;
     private SearchRecipeActivity.RecipeAdapter mAdapter;
     DatabaseReference mReference;
-    public ArrayList<Recipe>  list;
+    public ArrayList<Recipe> list;
 
     private List<Allergy> mAllergens;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setTitle("Search recipes");
 
         setContentView(R.layout.activity_search_recipe);
-        mSearchArea=findViewById(R.id.search_area);
-        mSearchResults=findViewById(R.id.search_results);
-        mSearchBtn=findViewById(R.id.search_imgbtn);
+        mSearchArea = findViewById(R.id.search_area);
+        mSearchResults = findViewById(R.id.search_results);
+        mSearchBtn = findViewById(R.id.search_imgbtn);
         mSearchResults.setLayoutManager(new LinearLayoutManager(SearchRecipeActivity.this));
         mSearchBtn.setOnClickListener(this);
 
-        mAllergens= LitePal.select("allergyTypeName","allergy_1","allergy_2").where("isChecked = ?","1").find(Allergy.class);
+        mAllergens = LitePal.select("allergyTypeName", "allergy_1", "allergy_2").where("isChecked = ?", "1").find(Allergy.class);
 
-        list=new ArrayList<Recipe>();
+        list = new ArrayList<Recipe>();
 
         mAdapter = new SearchRecipeActivity.RecipeAdapter(list);
         mSearchResults.setAdapter(mAdapter);
 
     }
+
+
     @Override
-    public void onClick(View view){
-        if(!mSearchArea.getText().toString().isEmpty()){
-            String searchText=mSearchArea.getText().toString().toLowerCase();
-            RecipeSearch(searchText);}
-        else{
-            Toast.makeText(this,"Please enter a text",Toast.LENGTH_LONG).show();
+    public void onClick(View view) {
+        String keyword = mSearchArea.getText().toString();
+        if (!mSearchArea.getText().toString().isEmpty()) {
+            String searchText = mSearchArea.getText().toString().toLowerCase();
+            RecipeSearch(searchText);
+        } else {
+            Toast.makeText(this, "Please enter a text", Toast.LENGTH_LONG).show();
         }
     }
 
 
-    protected void RecipeSearch(final String searchText){
+    protected void RecipeSearch(final String searchText) {
         list.clear();
+        final String[] subKeywords = searchText.split("\\p{Blank}|-|,|;");
 
-        mReference= FirebaseDatabase.getInstance().getReference().child("recipes");
+        mReference = FirebaseDatabase.getInstance().getReference().child("recipes");
         mReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if(!dataSnapshot.exists()){
+                if (!dataSnapshot.exists()) {
                     Toast.makeText(SearchRecipeActivity.this,
                             "No recipes found", Toast.LENGTH_SHORT).show();
                     return;
                 }
-                for(DataSnapshot recipeSnapshot : dataSnapshot.getChildren()){
-                    Recipe recipe=recipeSnapshot.getValue(Recipe.class);
-                    int toAdd= 1;
 
-                    for(int i = 0; i<mAllergens.size(); i++){
-                        Log.d(TAG,"sd");
-                        String mKey=mAllergens.get(i).getAllergyTypeName().toLowerCase()+mAllergens.get(i).getAllergy_1().toLowerCase()+mAllergens.get(i).getAllergy_2().toLowerCase();
-                        if(mKey.contains(recipe.getIngredient1())||mKey.contains(recipe.getIngredient2())||mKey.contains(recipe.getIngredient3())){
-                            toAdd=0;
+                for (DataSnapshot recipeSnapshot : dataSnapshot.getChildren()) {
+                    Recipe recipe = recipeSnapshot.getValue(Recipe.class);
+
+                    int toAdd = 1;
+                    for (int i = 0; i < mAllergens.size(); i++) {
+                        Log.d(TAG, "Search Recipe");
+                        String mKey = mAllergens.get(i).getAllergyTypeName().toLowerCase() + mAllergens.get(i).getAllergy_1().toLowerCase() + mAllergens.get(i).getAllergy_2().toLowerCase();
+                        if (mKey.contains(recipe.getIngredient1()) || mKey.contains(recipe.getIngredient2()) || mKey.contains(recipe.getIngredient3()) || mKey.contains(recipe.getRecipeName())) {
+                            toAdd = 0;
                             break;
                         }
                     }
 
-                    if(toAdd ==1&&(recipe.getRecipeName().contains(searchText)||recipe.getIngredient1().contains(searchText)||recipe.getIngredient2().contains(searchText)||recipe.getIngredient3().contains(searchText))){
-                        list.add(recipe);
+                    if (toAdd == 1) {
+                        for (String subKeyword : subKeywords) {
+                            if ((recipe.getRecipeName().toLowerCase().matches(".*" + subKeyword.toLowerCase() + ".*")) || (recipe.getIngredient1().toLowerCase().matches(".*" + subKeyword.toLowerCase() + ".*"))|| (recipe.getIngredient2().toLowerCase().matches(".*" + subKeyword.toLowerCase() + ".*"))|| (recipe.getIngredient3().toLowerCase().matches(".*" + subKeyword.toLowerCase() + ".*")))
+                            {
+                                list.add(recipe);
+                            }
+                        }
                     }
                 }
 
-                if(list.size()==0){
+                if (list.size() == 0) {
                     Toast.makeText(SearchRecipeActivity.this,
-                            "No recipes found", Toast.LENGTH_SHORT).show();
+                            "No recipes found :(", Toast.LENGTH_SHORT).show();
                 }
                 mAdapter.notifyDataSetChanged();
-
-
-
             }
 
             @Override
@@ -109,14 +118,13 @@ public class SearchRecipeActivity extends AppCompatActivity implements View.OnCl
     }
 
 
-
-    private class RecipeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    private class RecipeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView mNameTextView;
         private ImageView mImageView;
         private Recipe mRecipe;
 
-        public RecipeHolder(LayoutInflater inflater, ViewGroup parent){
-            super(inflater.inflate(R.layout.list_item_recipe,parent,false));
+        public RecipeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_recipe, parent, false));
             itemView.setOnClickListener(this);
             mNameTextView = (TextView) itemView.findViewById(R.id.recipe_name);
             mImageView = (ImageView) itemView.findViewById(R.id.recipe_image);
@@ -124,40 +132,41 @@ public class SearchRecipeActivity extends AppCompatActivity implements View.OnCl
         }
 
         @Override
-        public void onClick(View view){
-            Intent intent=RecipeActivity.newIntent(SearchRecipeActivity.this,mRecipe.getRecipeId(),mRecipe.getRecipeName(),mRecipe.getIngredient1(),mRecipe.getIngredient2(),mRecipe.getIngredient3(),mRecipe.getImageId());
+        public void onClick(View view) {
+            Intent intent = RecipeActivity.newIntent(SearchRecipeActivity.this, mRecipe.getRecipeId(), mRecipe.getRecipeName(), mRecipe.getIngredient1(), mRecipe.getIngredient2(), mRecipe.getIngredient3(), mRecipe.getImageId());
             startActivity(intent);
         }
 
-        public void bind(Recipe recipe){
+        public void bind(Recipe recipe) {
             mRecipe = recipe;
             mNameTextView.setText(mRecipe.getRecipeName());
             Picasso.with(SearchRecipeActivity.this).load(recipe.getImageId()).placeholder(R.drawable.default_recipe_pic).fit().into(mImageView);
         }
     }
 
-    private class RecipeAdapter extends RecyclerView.Adapter<SearchRecipeActivity.RecipeHolder>{
+    private class RecipeAdapter extends RecyclerView.Adapter<SearchRecipeActivity.RecipeHolder> {
         private List<Recipe> mRecipes;
+
         public RecipeAdapter(List<Recipe
-                > recipes){
-            mRecipes=recipes;
+                > recipes) {
+            mRecipes = recipes;
 
         }
 
         @Override
-        public SearchRecipeActivity.RecipeHolder onCreateViewHolder(ViewGroup parent, int viewType){
-            LayoutInflater layoutInflater=LayoutInflater.from(SearchRecipeActivity.this);
-            return new SearchRecipeActivity.RecipeHolder(layoutInflater,parent);
+        public SearchRecipeActivity.RecipeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(SearchRecipeActivity.this);
+            return new SearchRecipeActivity.RecipeHolder(layoutInflater, parent);
         }
 
         @Override
-        public void onBindViewHolder(SearchRecipeActivity.RecipeHolder holder, int position){
-            Recipe recipe=mRecipes.get(position);
+        public void onBindViewHolder(SearchRecipeActivity.RecipeHolder holder, int position) {
+            Recipe recipe = mRecipes.get(position);
             holder.bind(recipe);
         }
 
         @Override
-        public int getItemCount(){
+        public int getItemCount() {
             return mRecipes.size();
         }
     }
